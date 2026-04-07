91c42681e5b76de635176695de0c8daa8ca2d0fb
Sammy Hansali <sammyhansali@gmail.com>
2026-04-07 19:52:46 -0400

    setup tf iam role config for glue

---
+++ terraform/6-iam.tf
@@ -0,0 +1,25 @@
+resource "aws_iam_role" "glue_role" {
+    name = "glue_role"
+    assume_role_policy = jsonencode({
+        "Version": "2012-10-17",
+        "Statement": [
+            {
+                "Effect": "Allow",
+                "Principal": {
+                    "Service": "glue.amazonaws.com"
+                },
+                "Action": "sts:AssumeRole"
+            }
+        ]
+    })
+}
+
+resource "aws_iam_role_policy_attachment" "a" {
+  role       = aws_iam_role.glue_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
+}
+
+resource "aws_iam_role_policy_attachment" "b" {
+  role       = aws_iam_role.glue_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSGlueServiceRole"
+}
