5ddae2f5c96fcb06c9781ccdd69c93ad1f61b126
Sammy Hansali <sammyhansali@gmail.com>
2026-04-07 18:42:09 -0400

    added resources for vpc and sgs

---
+++ terraform/2-vpc.tf
@@ -0,0 +1,40 @@
+resource "aws_vpc" "main" {
+    cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "one" {
+    vpc_id = aws_vpc.main.id
+    cidr_block = "10.0.1.0/24"
+    availability_zone = "us-east-1a"
+    map_public_ip_on_launch = true
+}
+
+resource "aws_subnet" "two" {
+    vpc_id = aws_vpc.main.id
+    cidr_block = "10.0.2.0/24"
+    availability_zone = "us-east-1b"
+    map_public_ip_on_launch = true
+}
+
+resource "aws_internet_gateway" "gw" {
+    vpc_id = aws_vpc.main.id
+}
+
+resource "aws_route_table" "main" {
+    vpc_id = aws_vpc.main.id
+
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.gw.id
+    }
+}
+
+resource "aws_route_table_association" "one" {
+    subnet_id = aws_subnet.one.id
+    route_table_id = aws_route_table.main.id
+}
+
+resource "aws_route_table_association" "two" {
+    subnet_id = aws_subnet.two.id
+    route_table_id = aws_route_table.main.id
+}
